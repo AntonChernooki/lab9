2c73e9a8a054f4bfeda0136cfb9b1dd1875ac423
AntonChernooki <119884716+AntonChernooki@users.noreply.github.com>
2022-12-06 23:24:45 +0300

    Добавлен вывод общей информации о задании

---
+++ lab9/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #2. Conference program\n";
+    cout << "Author: ChernookiAnton\n";
+    return 0;
+}
